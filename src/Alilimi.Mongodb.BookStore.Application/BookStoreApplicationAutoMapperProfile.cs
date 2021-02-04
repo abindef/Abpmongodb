48bee16de06c70e8b5ab4cc711fb70c620e7fe92
abindef <abindef@163.com>
2021-02-04 14:46:38 +0800

    增加Books实体CRUD

--- src/Alilimi.Mongodb.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ src/Alilimi.Mongodb.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Alilimi.Mongodb.BookStore.Books;
+using AutoMapper;
 
 namespace Alilimi.Mongodb.BookStore
 {
@@ -9,6 +10,8 @@ namespace Alilimi.Mongodb.BookStore
             /* You can configure your AutoMapper mapping configuration here.
              * Alternatively, you can split your mapping configurations
              * into multiple profile classes for a better organization. */
+            CreateMap<Book, BookDto>();
+            CreateMap<CreateUpdateBookDto, Book>();
         }
     }
 }
